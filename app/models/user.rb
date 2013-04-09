a87007e9bbc1f5f3f4442e53f2a09caec7d00d08
Vishal Sakaria <vsakaria@gmail.com>
2013-04-09 10:25:16 +0100

    omniauth sign in with twitter added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,21 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :omniauthable,
-         :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :name
+  attr_accessible  :name, :email, :password, :provider, :uid
 
   validates :name, :presence => true
 
   has_many :comments
 
-def self.from_omniauth(auth)
-  where(auth.slice(:provider, :uid)).first_or_create do |user|
-    user.provider = auth.provider
-    user.uid = auth.uid
-    user.username = auth.info.nickname
+  def self.from_omniauth(auth)
+    where(auth.slice("provider","uid")).first || create_from_omniauth(auth)
   end
-end
 
-def self.new_with_session(params, session)
-  if session["devise.user_attributes"]
-    new(session["devise.user_attributes"], without_protection: true) do |user|
-      user.attributes = params
-      user.valid?
+  def self.create_from_omniauth(auth)
+    self.create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
     end
-  else
-    super
   end
-end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,37 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130408102955) do
-
-  create_table "comments", :force => true do |t|
-    t.string   "comment"
-    t.boolean  "answer"
-    t.integer  "rightanswers"
-    t.integer  "wronganswers"
-    t.integer  "user_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-  end
+ActiveRecord::Schema.define(:version => 20130409082746) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "email"
+    t.string   "password"
     t.string   "provider"
     t.string   "uid"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
