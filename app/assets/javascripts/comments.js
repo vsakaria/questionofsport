16a1447c865ebc21f9c0e60558913634c808895a
Rich Wisniewski <richwisniewski@gmail.com>
2013-04-10 22:47:36 +0100

    main question div styling. logo tweaks. general tidy-up

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
   })
 
   $('#quiz-trigger').on('click', function (){
+    // $('.ask-questions').removeClass(.'ask-questions');
     $('#questions').slideToggle('400');
   })
 
